133bdd6f2c54d033fb3d427175b7bd1a33b7365e
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-22 18:20:40 -0600

    m,ade some changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Function 1
 function multiplyByFive(figure1, figure2) {
     let number = figure1 + figure2;
-    const result = number * 5;
+    let result = number * 5;
     return result;
   }
   
   // Function 2
-  function BiggerThanTen(string) {
+  function GreaterThanTen(string) {
     if (string.length > 10) {
       return true;
     } else {
@@ -15,10 +15,10 @@ function multiplyByFive(figure1, figure2) {
   }
   
 
-  console.log(multiplyByFive(2, 3)); // should log 25 since (5 * (2 + 3) = 25)
-  console.log(multiplyByFive(10, 20)); // should log 150 since(5 * (10 + 20) = 150)
-  console.log(multiplyByFive(-5, 7)); // should log 10 since (5 * (-5 + 7) = 10)
+  console.log(multiplyByFive(2, 3)); // should log 25 since (5 * (2 + 3) = 25);
+  console.log(multiplyByFive(10, 20)); // should log 150 since(5 * (10 + 20) = 150);
+  console.log(multiplyByFive(-5, 7)); // should log 10 since (5 * (-5 + 7) = 10);
 
-console.log(GreaterThanTen("hello world")); // should log true because 11 is greater tha 10(11 > 10)
-console.log(GreaterThanTen("hi")); // should log false because 2 is lesser than 10 (2 <= 10)
-console.log(GreaterThanTen("1234567890")); // should log false because 10 is not greater than 10  (10 <= 10)
+console.log(GreaterThanTen("hello world")); // should log true because 11 is greater than 10 (11 => 10);
+console.log(GreaterThanTen("hi")); // should log false because 2 is lesser than 10 (2 <= 10);
+console.log(GreaterThanTen("alex")); // should log false because 10 is not greater than 10  (4 <= 10);
